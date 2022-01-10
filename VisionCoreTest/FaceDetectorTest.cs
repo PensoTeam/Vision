11d76a1b7c610dcf91dc07f83aa516a37ad1d095
Juhan <hanchaa@gmail.com>
2022-01-11 00:06:52 +0900

    feat(face_detect): add face detector test

---
+++ VisionCoreTest/FaceDetectorTest.cs
@@ -0,0 +1,31 @@
+﻿using OpenCvSharp;
+using VisionCore;
+using Xunit;
+
+namespace VisionCoreTest
+{
+    public class FaceDetectorTest
+    {
+        private readonly FaceDetector _faceDetector;
+        
+        public FaceDetectorTest()
+        {
+            _faceDetector = new FaceDetector();
+        }
+
+        ~FaceDetectorTest()
+        {
+            _faceDetector.Dispose();
+        }
+        
+        [Fact]
+        public void TestFaceNum()
+        {
+            using (var img = Cv2.ImRead("../../../../VisionCoreTest/data/Lenna.png"))
+            {
+                var faces = _faceDetector.GetFacePositions(img);
+                Assert.Single(faces);
+            }
+        }
+    }
+}
